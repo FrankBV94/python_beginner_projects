63ecf4374c22cba52debcca04834f187d5b6d398
FrankBV94 <frankbotelle@gmail.com>
2022-09-06 14:58:58 -0400

    Revisar

---
+++ 7-qr-code-generator/app.py
@@ -0,0 +1,6 @@
+# install all the libraries needed
+# create a function that collets a text and convert it to a qrcode
+# save the qrcode as an image
+# run the function
+
+# TODO: Arreglar por que no se instalan los paquetes 
